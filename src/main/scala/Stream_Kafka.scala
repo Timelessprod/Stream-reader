a7ae1f206db454fde714f43bf5e7e1dee69ac35c
Erwan <wan.goudard@gmail.com>
2022-06-01 20:52:55 +0200

    [FIX]: didn't compile

--- src/main/scala/Stream_Kafka.scala
+++ src/main/scala/Stream_Kafka.scala
@@ -1,5 +1,4 @@
 import org.apache.kafka.streams.scala.Serdes._
-import com.goyeau.kafka.streams.circe.CirceSerdes._
 
 class Stream_Kafka(input: DroneReport) {
     // creation builder
@@ -17,5 +16,5 @@ class Stream_Kafka(input: DroneReport) {
     val producer = Producer[DroneReport](config)
 
     // etapes pour envoyer un report (a commenter car la ca le fait 1 fois pour l'exemple)
-    val droneRep = DroneReport("123456789012", "210987654321", "2016-01-01T00:00:00.000Z", 0.0, 0.0, Array("Hello", "I", "am", "a", "PeaceWatcher"), )
+    val droneRep = DroneReport(123456789, 987654321, "2016-01-01T00:00:00.000Z", 0.0, 0.0, Array("Hello", "I", "am", "a", "PeaceWatcher"), Map(Array(147852369, 10)))
 }
