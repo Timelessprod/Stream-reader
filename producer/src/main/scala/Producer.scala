f1686f6efc73822edbe637b6b1534383b2937701
Erwan <wan.goudard@gmail.com>
2022-06-09 15:46:58 +0200

    [FIX]: Producer is working git status!

--- producer/src/main/scala/Producer.scala
+++ producer/src/main/scala/Producer.scala
@@ -4,6 +4,9 @@ import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord, RecordMetadata}
 import org.apache.logging.log4j.{Logger, LogManager}
 import scala.collection.mutable.ArrayBuffer
+import org.apache.kafka.common.serialization.Serde
+
+
 object Producer {
     lazy val logger: Logger = LogManager.getLogger(getClass.getName)
 
@@ -11,10 +14,14 @@ object Producer {
 
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")    // il faudra plusieurs broker
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "SerdeDrone")
-    // props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, Serialization)
+    // props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "SerdeDrone")
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
 
     // récupère le json sous forme d'array de map des droneReports
+    // utilisation:
+    //     data : ArrayBuffer[ujson.Value]
+    //     data(0) : ujson.Value
+    //     data(0)("reportId") : Int (attribut reportId du premier report)
     def readJson(filename: String) = {
         val jsonString = Source.fromFile(filename).getLines().mkString
         val data = ujson.read(jsonString)
@@ -22,8 +29,8 @@ object Producer {
     }
 
     // envoie UN report dans la stream
-    def sendReport(droneReport: ujson.Value, producer: KafkaProducer[String, SerdeDrone]) = {
-        val record = new ProducerRecord[String, SerdeDrone]("drone-report", droneReport("reportId").toString, new SerdeDrone(droneReport))
+    def sendReport(droneReport: ujson.Value, producer: KafkaProducer[String, String]) = {
+        val record = new ProducerRecord[String, String]("drone-report", droneReport("reportId").toString, droneReport.toString)
         producer.send(record, (metadata: RecordMetadata, exception: Exception) => {
             if (exception != null) {
                 exception.printStackTrace()
@@ -35,7 +42,7 @@ object Producer {
 
     // envoie tous les reports dans l'array dans la stream
     def sendReports(droneReports: ArrayBuffer[ujson.Value]) = {
-        val producer = new KafkaProducer[String, SerdeDrone](this.props)
+        val producer = new KafkaProducer[String, String](this.props)
 
         droneReports.foreach { record => sendReport(record, producer) }
         logger.info("Drone reports send")
@@ -47,12 +54,15 @@ object Producer {
     def run() = {
         // je sais pas comment ils sont rangés et créer là donc faudra vérifier
         val droneReports = readJson("../json/s1.json")
+        sendReports(droneReports)
 
 
+        /*println("test is testing")
         val test = readJson("../test.json")
-        /*println(test.getClass.getName)
+        println(test.getClass.getName)
         println(test(0).getClass.getName)
-        println(test(0).toString)*/
-        sendReports(test)
+        println(test(0).toString)
+        sendReports(test)*/
+        println("I did it ?!")
     }
 }
