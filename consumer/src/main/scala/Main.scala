a64c82a72150076870b5df161696590a4c557b12
Erwan <wan.goudard@gmail.com>
2022-06-11 00:38:31 +0200

    fix

--- consumer/src/main/scala/Main.scala
+++ consumer/src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 object Main {
     def main(args: Array[String]): Unit = {
-        // ConsumerSpark.consumeAndWrite()
-        Consumer.run()
+        ConsumerSpark.consumeAndWrite()
+        // Consumer.run()
     }
 }
