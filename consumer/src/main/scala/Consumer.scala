1475d90e66c85b9ac9021279d8821dc8f42cca96
Erwan <wan.goudard@gmail.com>
2022-06-10 17:22:04 +0200

    WiP

--- consumer/src/main/scala/Consumer.scala
+++ consumer/src/main/scala/Consumer.scala
@@ -1,53 +1,3 @@
-/*<<<<<<< HEAD
-import java.util
-import java.util.Properties
-import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecords, KafkaConsumer}
-import org.apache.logging.log4j.{LogManager, Logger}
-import scala.collection.JavaConverters._
-// import scala.jdk.CollectionConverters._
-import java.time.Duration
-
-
-object Consumer {
-    lazy val logger: Logger = LogManager.getLogger(getClass.getName)
-    val props = new Properties()
-
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put(ConsumerConfig.GROUP_ID_CONFIG, "0001")
-
-    
-    def receiveReport(consumer: KafkaConsumer[String, String]): Unit = {
-        println("test 1")
-        val records: ConsumerRecords[String, String] = consumer.poll(Duration.ofMillis(100))
-        println("test 2")
-        records.asScala.foreach { record =>
-            println(s"offset = ${record.offset()}, key = ${record.key()}, value = ${record.value()}")}
-        println("test 3")
-        
-        logger.info(s"${records.count()} report(s) received")
-        println(s"${records.count()} report(s) received")
-
-        println("test 4")
-        consumer.commitSync()
-
-        receiveReport(consumer)
-        println("test 5")
-        
-    }
-
-    def run() = {
-        val topic = "drone-report"
-        val consumer = new KafkaConsumer[String, String](this.props)
-        consumer.subscribe(util.Collections.singletonList(topic))
-
-        println("Starting consuming")
-        receiveReport(consumer)
-        println("End of consuming")
-    }
-
-=======*/
 import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord, ConsumerRecords, KafkaConsumer}
 import io.circe._
 import io.circe.parser._
@@ -92,6 +42,8 @@ object Consumer {
 
         })
         logger.info(s"${records.count()} report(s) received")
+
+        consumer.commitSync()
 
         receiveReport(consumer)
     }
