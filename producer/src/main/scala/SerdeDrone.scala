395e6bc6bf75584035325a75262923d5f2548434
Adrien Merat <adrien.merat@epita.fr>
2022-06-11 23:58:59 +0200

    [DOC] Add documentation to the code

--- producer/src/main/scala/SerdeDrone.scala
+++
@@ -1,7 +0,0 @@
-import java.io.Serializable
-import ujson._
-
-@SerialVersionUID(100L)
-class SerdeDrone(value: ujson.Value) extends Serializable {
-  override def toString = f"${value.toString}"
-}
