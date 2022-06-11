9dc8015693c1247858df41193b53ef043caee98a
Corentin Duchêne <corentin.duchene@epita.fr>
2022-06-11 19:53:41 +0200

    [ADD] plot example in Main

--- analyzer/src/main/scala/Main.scala
+++ analyzer/src/main/scala/Main.scala
@@ -1,3 +1,19 @@
+import vegas._
+import vegas.render.WindowRenderer._
+
 object Main extends App {
-  println("Hello, World!")
+  val plot = Vegas("Country Pop").
+    withData(
+      Seq(
+        Map("country" -> "USA", "population" -> 314),
+        Map("country" -> "UK", "population" -> 64),
+        Map("country" -> "DK", "population" -> 80)
+      )
+    ).
+    encodeX("country", Nom).
+    encodeY("population", Quant).
+    mark(Bar)
+
+  //plot.window.show
+  println(plot.html.pageHTML())
 }
