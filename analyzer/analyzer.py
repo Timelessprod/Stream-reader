67dcdfbaa25b578e9b63283a8cc93b8e34737255
Adrien Merat <adrien.merat@epita.fr>
2022-06-12 00:37:01 +0200

    [FIX]

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -2,24 +2,32 @@ from pyspark.sql import SparkSession
 import matplotlib.pyplot as plt
 from datetime import datetime
 import calendar
+import pyspark.sql.functions as F
 
+def rename_columns(df, columns):
+    if isinstance(columns, dict):
+        return df.select(*[F.col(col_name).alias(columns.get(col_name, col_name)) for col_name in df.columns])
+    else:
+        raise ValueError("fail")
 
 def get_data():
-    spark = SparkSession.builder().appName("analyzer").getOrCreate()
+    spark = SparkSession.builder.appName("analyzer").getOrCreate()
     df = spark.read.csv("hdfs://localhost:9000/drone-reports")
-    return df
+    names = {"_c0": "reportId", "_c1": "peaceWatcherId", "_c2": "timestamp", "_c3": "longitude", "_c4": "latitude", "_c5": "hearWords", "_c6": "citizenId", "_c7": "peaceScores"}
+    df = df.transform(lambda df: rename_columns(df, names))
+    return df.toPandas()
 
 def get_only_alerts(df):
-    return df.filter(df.peaceScores < 50)
+    return df[df["peaceScores"].astype(int) > 50]
 
 def graph_location(df):
     df = get_only_alerts(df)
     fig, ax = plt.subplots()
-    ax.scatter(df.longitude, df.latitude)
+    ax.scatter(df.longitude.astype(float), df.latitude.astype(float))
     fig.show()
 
 def get_hour(df):
-    df = datetime.strptime(df.timestamp, "%Y-%m-%d %H:%M:%S").hour
+    df = datetime.strptime(df.timestamp.astype(str), "%Y-%m-%d %H:%M:%S").hour
     heights = [0 for i in range(25)]
     for elem in df:
         heights[elem.tointeger] += 1
@@ -34,7 +42,7 @@ def graph_time_day(df):
     fig.show()
 
 def get_day(df):
-    df = datetime.strptime(df.timestamp, "%Y-%m-%d %H:%M:%S")
+    df = datetime.strptime(df.timestamp.astype(str), "%Y-%m-%d %H:%M:%S")
     heights = [0 for i in range(7)]
     for elem in df:
         heights[elem.weekday()] += 1
@@ -52,7 +60,7 @@ def graph_evol_peacescore(df):
     df.withColumn("weekday", calendar.day_name[df.timestamp.weekday()])
     df = df.groupby("weekday").agg({"peaceScore":"avg"})
     fig, ax = plt.subplots()
-    ax.bar(df.weekday, df.peaceScore)
+    ax.bar(df.weekday, df.peaceScores)
     fig.show()
 
 
