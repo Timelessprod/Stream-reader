8f5ee35c461cc88586ecea6787e1d5932063f179
Corentin Duchêne <corentin.duchene@epita.fr>
2022-06-12 14:01:11 +0200

    [ADD] show drone-reports hdfs file

--- analyzer/src/main/scala/Main.scala
+++ analyzer/src/main/scala/Main.scala
@@ -1,32 +1,6 @@
-import vegas._
-import vegas.data.External._
-import vegas.render.WindowRenderer._
 import org.apache.spark.sql.SparkSession
 
 object Main extends App {
   val spark = SparkSession.builder().appName("analyzer").config("spark.master", "local[*]").getOrCreate()
-  import spark.implicits._
-
-  def readData() = {
-    val df = this.spark.read.options(Map("inferSchema"->"true","delimiter"->",","header"->"true"))
-  .csv("../json/s1.csv")
-    df.show
-    df
-  }
-  val df = readData()
-  val plot = Vegas("Lieux reports").
-    withData(
-      Seq(
-        
-        Map("country" -> "USA", "population" -> 314),
-        Map("country" -> "UK", "population" -> 64),
-        Map("country" -> "DK", "population" -> 80)
-      )
-    ).
-    encodeX("country", Nom).
-    encodeY("population", Quant).
-    mark(Bar)
-
-  // plot.window.show
-  // println(plot.html.pageHTML())
+  spark.read.csv("hdfs://localhost:9000/drone-reports").show()
 }
