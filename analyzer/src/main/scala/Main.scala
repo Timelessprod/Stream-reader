56eb0c0d52e18c1d80f775aaf723c6636b485818
Corentin Duchêne <corentin.duchene@epita.fr>
2022-06-12 22:03:43 +0200

    [CLEAN] remove old scala and python analyzers

--- analyzer/src/main/scala/Main.scala
+++
@@ -1,6 +0,0 @@
-import org.apache.spark.sql.SparkSession
-
-object Main extends App {
-  val spark = SparkSession.builder().appName("analyzer").config("spark.master", "local[*]").getOrCreate()
-  spark.read.csv("hdfs://localhost:9000/drone-reports").show()
-}
